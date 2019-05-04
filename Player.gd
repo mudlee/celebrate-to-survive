64667882e18b81730ec9941d8524244b43467d3e
Sandor Muranyi <contact@mudlee.hu>
2019-05-04 20:52:25 +0200

    basic ui logic, more platforms

--- Player.gd
+++ Player.gd
@@ -4,12 +4,16 @@ signal fireworks_active
 signal fireworks_not_active
 onready var fireworks = get_node("Fireworks")
 onready var fireworks_shards = get_node("Fireworks/Shards")
+onready var time_left_text = get_parent().get_node("Canvas/TimeLeftText")
+onready var fireworks_text = get_parent().get_node("Canvas/FireworksText")
 
 const FIREWORKS_PAUSE_LENGTH = 2.0
 const ACCELERATION = 10000.0
 const MAX_SPEED = 6000
 const GRAVITY = 12000.0
 const JUMP_FORCE = 14000.0
+const GAME_LENGTH = 99
+const FIREWORKS_LIMIT = 5
 
 var velocity = Vector2()
 var direction = 0
@@ -19,8 +23,12 @@ var speed_y = 0
 var jumping = false
 var fireworks_activated_time
 var fireworks_active = false
+var fireworks_last_used = 0
+var fireworks_ready = true
+var time_start = 0
 
 func _ready():
+	time_start = OS.get_unix_time()
 	for platform in get_parent().get_node("Platforms").get_children():
 		connect("fireworks_active", platform, "fireworks_active")
 		connect("fireworks_not_active", platform, "fireworks_not_active")
@@ -31,15 +39,28 @@ func _input(event):
 		speed_y = -JUMP_FORCE
 		jumping = true
 
-	if not fireworks_active and event.is_action_pressed("fireworks"):
+	if fireworks_ready && not fireworks_active and event.is_action_pressed("fireworks"):
 		fireworks.emitting = true
 		fireworks_shards.emitting = true
 		fireworks_activated_time = OS.get_ticks_msec()
 		fireworks_active = true
+		fireworks_last_used = OS.get_unix_time()
+		fireworks_text.add_color_override("default_color", Color(1,0,0,1))
+		fireworks_text.set_text("FIREWORKS NOT READY")
+		fireworks_ready = false
 		emit_signal("fireworks_active")
 
 
 func _process(delta):
+	var elapsed = (OS.get_unix_time() - time_start)%60
+	var remaining = GAME_LENGTH - elapsed
+	time_left_text.set_text("Time Left: "+str(remaining))
+
+	if fireworks_last_used == 0 or (OS.get_unix_time()-fireworks_last_used)%60 > FIREWORKS_LIMIT:
+		fireworks_ready = true
+		fireworks_text.add_color_override("default_color", Color(0,1,0,1))
+		fireworks_text.set_text("FIREWORKS READY")
+
 	if fireworks_active && float((OS.get_ticks_msec() - fireworks_activated_time))/1000.0 > FIREWORKS_PAUSE_LENGTH:
 		fireworks_active = false
 		emit_signal("fireworks_not_active")
